abedcb2fb7fad9f2755df2b68324e2f6146f8efc
Mike Kelly <pioto@pioto.org>
2006-06-06 22:19:48 +0000

    Added proper tabbing, and a check to see if the user yet exists
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@13 1c067ae2-9913-0410-8806-fa90ff404265

--- bash/auth/compat.sh
+++ bash/auth/compat.sh
@@ -9,7 +9,17 @@
 # NEWUSER: the username to be added
 # 
 adduser_compat() {
-    # TODO: Add code to determine a free UID here
+	# See if the user already exists
+	if [[ ${NEWUSER} == $(egetent passwd "${NEWUSER}" | cut -d: -f1) ]] ;
+	then
+		# TODO: If there needs to be some change made, note it
+		# properly, for the user to take care of later with
+		# the eselect tool
+		return 0
+	fi
+
+	# TODO: Add code to determine a free UID here
+		
 
     # For plain-old Gentoo/Linux
     echo We\'d be doing: useradd -d \"${userhome}\" -G \"${usergroups}\" -s \"${usershell}\" -u \"${userid}\" -c \"${usercomment}\" \"${NEWUSER}\"
