831ffb3372d1ec79f779807347cce45a8cf8713c
Mike Kelly <pioto@pioto.org>
2006-06-08 01:20:28 +0000

    added skeleton database code
    took userdata parsing out of main adduser script (so that it is easier 
    to port to other distros, etc)
    
    
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@18 1c067ae2-9913-0410-8806-fa90ff404265

--- bash/adduser.sh
+++ bash/adduser.sh
@@ -10,65 +10,48 @@
 
 # Read the command line arguments.
 
-# The only option recognized now is the user to be added.
+# The only options recognized now are the user to be added and the 
+# package which is doing the adding.
 NEWUSER="${1}"
+PKGNAME="${2}"
+
 ###
 
 # Read the proper data file for the desired user
+. common/read_userdata.sh
 
-# In the full implementation, should go through all the cascading
-# profiles in proper order, but for now we're just parsing these 2
-# config files
+# Now, we verify this information against our database, and if there
+# isn't a match (either some value has changed, or there is no entry
+# at all), we take appropriate action
 
-# TODO: Build USERFILE dynamically based upon the currently selected profile.
-# Work from the profile up to its parents, etc, to build this list.
-USERFILE="${DATADIR}/user/${NEWUSER} ${DATADIR}/accounts"
+if [[ -f "${DBDIR}/users/${NEWUSER}" ]] ; then
+	# So, we know this user already exists. At the end of this 
+	# script, we need to be sure to add the current package to this 
+	# file.
 
-for i in ${USERFILE}; do
-	userid_=$(grep ^uid: ${i} |cut -d: -f2)
-	userid_=${userid_## }
-	userid=${userid:-${userid_}}
+	# TODO: We need to see if the user matches what it should. If 
+	# not, inform the operator of what changes must be made.
+	echo "User already exists. Doing nothing [for now]"
+else
+	# Finally, we take take the necessary action, either via usermod 
+	# or useradd [or it's comparable friends]
 
-	usershell_=$(grep ^shell: "${i}" |cut -d: -f2)
-	usershell_=${usershell_## }
-	usershell=${usershell:-${usershell_}}
+	## TODO: Add code for knowing when we are modifying, not adding
 
-	userhome_=$(grep ^home: "${i}" |cut -d: -f2)
-	userhome_=${userhome_## }
-	userhome=${userhome:-${userhome_}}
+	for i in ${PASSWD_BACKENDS}; do
+		case ${i} in
+			compat)
+				. "auth/${i}.sh"
+				adduser_compat
+				;;
+			*)
+				echo -n "ERROR: Auth mechanism \"${i}\""
+				echo	" not supported by this script."
+				return 1
+				;;
+		esac
+	done
+fi
 
-	usergroups_=$(grep ^groups: "${i}" |cut -d: -f2)
-	usergroups_=${usergroups_## }
-	usergroups=${usergroups:-${usergroups_}}
-
-	usercomment_=$(grep ^comment: "${i}" |cut -d: -f2)
-	usercomment_=${usercomment_## }
-	usercomment=${usercomment:-${usercomment_}}
-
-	unset userid_ usershell_ userhome_ usergroups_ usercomment_
-done
-
-# Now, we would verify this information against our database, and if
-# there isn't a match [either some value has changed, or there is no
-# entry at all], we take appropriate action
-
-## TODO: Insert database checking code here
-
-# Finally, we take take the necessary action, either via usermod or
-# useradd [or it's comparable friends]
-
-## TODO: Add code for knowing when we are modifying, not adding
-
-for i in ${PASSWD_BACKENDS}; do
-	case ${i} in
-		compat)
-			. "auth/${i}.sh"
-			adduser_compat
-			;;
-		*)
-			echo -n "ERROR: Auth mechanism \"${i}\" not supported"
-			echo    "by this script."
-			return 1
-			;;
-	esac
-done
+# Properly update the dynamic users database
+echo "${PKGNAME}" >> "${DBDIR}/users/${NEWUSER}"
--- bash/common/config.sh
+++ bash/common/config.sh
@@ -12,7 +12,19 @@ NSSWITCH_CONF="/etc/nsswitch.conf"
 # In reality this should be the "accounts" directory of their portage
 # profile, at least in Gentoo. Other distros would use an appropriate
 # directory here.
+#DATADIR="/etc/make.profile"
 DATADIR="../data"
+
+# DBDIR: The directory which contains the database of auto-added users. 
+# It is made up of a list of files in 2 subdirs, users and groups, named 
+# for the added user or group. Each line of the file contains the full 
+# package name and version as given by the package management system. 
+# When a package is uninstalled, it should be removed from any of these 
+# files. Then, the userspace update-users [or similar] script can 
+# properly know when a dynamically-added user can be removed from the 
+# system.
+#DBDIR="/var/db/dynusers"
+DBDIR="../db"
 
 # Get a list of the backend databases we're going to use
 ## TODO: Make these regexps better, more general
