f59c15c2fcc09363af7e2d85be8ef86fdb7fae3f
Mike Kelly <pioto@pioto.org>
2006-06-09 01:49:35 +0000

    cleanup & reorginization
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@19 1c067ae2-9913-0410-8806-fa90ff404265

--- bash/adduser.sh
+++ bash/adduser.sh
@@ -24,6 +24,30 @@ PKGNAME="${2}"
 # isn't a match (either some value has changed, or there is no entry
 # at all), we take appropriate action
 
+# We make use of egetent:
+. common/getent.sh
+
+# See if the user already exists
+if [[ ${NEWUSER} == $(egetent passwd "${NEWUSER}" | cut -d: -f1) ]] ;
+then
+	# TODO: If there needs to be some change made, note it
+	# properly, for the user to take care of later with
+	# the eselect tool
+	return 0
+fi
+# TODO: Add code to determine a free UID here
+uidmin=${userid%-*}
+uidmax=${userid#*-}
+
+if [[ -z $(egetent passwd ${uidmin}) ]] ; then
+	userid=${uidmin}
+else
+	userid=$(( ${usermin} + 1))
+	for i in $( seq ${userid} ${uidmax} ) ; do
+		[[ -z $(egetent passwd ${i}) ]] && userid=${i} && break
+	done
+fi
+
 if [[ -f "${DBDIR}/users/${NEWUSER}" ]] ; then
 	# So, we know this user already exists. At the end of this 
 	# script, we need to be sure to add the current package to this 
--- bash/auth/compat.sh
+++ bash/auth/compat.sh
@@ -2,38 +2,14 @@
 #
 # $Id$
 
-# We make use of egetent:
-. common/getent.sh
-
 # adduser_compat() - adds a user to the system using the "compat" backend.
 #
 # Expects the following variables to already be set:
 #
 # NEWUSER: the username to be added
 # 
+# Expects a valid userid to have already been determined
 adduser_compat() {
-	# See if the user already exists
-	if [[ ${NEWUSER} == $(egetent passwd "${NEWUSER}" | cut -d: -f1) ]] ;
-	then
-		# TODO: If there needs to be some change made, note it
-		# properly, for the user to take care of later with
-		# the eselect tool
-		return 0
-	fi
-
-	# TODO: Add code to determine a free UID here
-	uidmin=${userid%-*}
-	uidmax=${userid#*-}
-
-	if [[ -z $(egetent passwd ${uidmin}) ]] ; then
-		userid=${uidmin}
-	else
-		userid=$(( ${usermin} + 1))
-		for i in $( seq ${userid} ${uidmax} ) ; do
-			[[ -z $(egetent passwd ${i}) ]] && userid=${i} && break
-		done
-	fi
-
 	# For plain-old Gentoo/Linux
 	echo We\'d be doing: useradd -d \"${userhome}\" -G \
 	\"${usergroups}\" -s \"${usershell}\" -u \"${userid}\" -c \
