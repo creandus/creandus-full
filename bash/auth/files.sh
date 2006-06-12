8bf50e68f70180281aacd46f39a0bc849e6779f6
Mike Kelly <pioto@pioto.org>
2006-06-12 23:33:28 +0000

    moving currently developing stuff to "trunk"
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@20 1c067ae2-9913-0410-8806-fa90ff404265

---
+++ bash/auth/files.sh
@@ -0,0 +1,17 @@
+# auth/files.sh - "files" backend functions
+#
+# $Id$
+
+# adduser_files() - adds a user to the system using the "compat" backend.
+#
+# Expects the following variables to already be set:
+#
+# NEWUSER: the username to be added
+# 
+# Expects a valid userid to have already been determined
+adduser_files() {
+	# For plain-old Gentoo/Linux
+	echo We\'d be doing: useradd -d \"${userhome}\" -G \
+	\"${usergroups}\" -s \"${usershell}\" -u \"${userid}\" -c \
+	\"${usercomment}\" \"${NEWUSER}\"
+}
