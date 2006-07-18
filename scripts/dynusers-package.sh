52f63f2a38c6c9c247fdae2271011ed5156cf88b
Mike Kelly <pioto@pioto.org>
2006-07-18 23:35:38 +0000

    Add a script to help automate the testing / release process.
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@67 1c067ae2-9913-0410-8806-fa90ff404265

---
+++ scripts/dynusers-package.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+REPODIR=${HOME}/svn/glep0027/trunk
+MODULES=
+WORKDIR=.
+
+cd ${WORKDIR}
+
+echo ">>> Building list of dynusers modules: "
+for i in `/bin/ls -d ${REPODIR}/*/` ; do
+	if [[ -d ${i} ]] ; then
+		i=`basename ${i/\/}`
+		echo "  + ${i}"
+		MODULES="${MODULES} ${i}"
+	fi
+done
+
+echo ">>> Removing old trees..."
+rm -rf ${MODULES}
+
+echo ">>> Copying current tree..."
+mkdir ${MODULES}
+for i in ${MODULES} ; do
+	echo "  + ${i}"
+	cd ${i}
+	cp -r ${REPODIR}/${i}/* .
+	find -type d -name .svn |xargs rm -rf
+	cd -
+done
+
+echo ">>> Running ./autogen.bash..."
+for i in ${MODULES} ; do
+	test -f ${i}/autogen.bash || continue
+	echo "  + ${i}"
+	cd ${i}
+	./autogen.bash || exit ${?}
+	cd -
+done
+
+echo ">>> Running ./configure..."
+for i in ${MODULES} ; do
+	test -f ${i}/configure || continue
+	echo "  + ${i}"
+	cd ${i}
+	./configure || exit ${?}
+	cd -
+done
+
+echo ">>> Running make..."
+for i in ${MODULES} ; do
+	test -f ${i}/Makefile || continue
+	echo "  + ${i}"
+	cd ${i}
+	make || exit ${?}
+	cd -
+done
+
+echo ">>> Running make distcheck..."
+for i in ${MODULES} ; do
+	test -f ${i}/Makefile || continue
+	echo "  + ${i}"
+	cd ${i}
+	make distcheck || exit ${?}
+	cd -
+done
+
+echo "All tests succeeded! :)"
