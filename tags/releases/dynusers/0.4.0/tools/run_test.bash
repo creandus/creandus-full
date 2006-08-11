c9c09222beac9de46898606ebef03c55721b67be
Mike Kelly <pioto@pioto.org>
2006-08-11 04:36:33 +0000

    Tag dynusers 0.4.0
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@165 1c067ae2-9913-0410-8806-fa90ff404265

---
+++ tags/releases/dynusers/0.4.0/tools/run_test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Copyright 2006 Mike Kelly
+# Distributed under the terms of the GNU General Public License v2
+#
+# Runs tests on all the frontend scripts, mainly intending to catch syntax
+# errors.
+#
+# $Id$
+
+echo "Testing \"${1}\":"
+bash ${1} --test
+# vim: ts=4 :
