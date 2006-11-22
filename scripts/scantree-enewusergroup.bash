573c124ef673dc7be69c904f3cdcc9b7e7b89a36
Mike Kelly <pioto@pioto.org>
2006-11-22 17:43:09 +0000

    Cleanup the scantree script. Give some progress output.
    git-svn-id: file:///tank/backups/everglades/var/svn/creandus@226 1c067ae2-9913-0410-8806-fa90ff404265

--- scripts/scantree-enewusergroup.bash
+++ scripts/scantree-enewusergroup.bash
@@ -3,51 +3,63 @@ PORTDIR="${PORTDIR:-$(paludis --configuration-variable gentoo location)}"
 PORTDIR="${PORTDIR:-$(portageq envvar PORTDIR)}"
 PORTDIR="${PORTDIR:-/usr/portage}"
 
-echo "Creating temp files:" 1>&2
-
+echo -n "Creating temp files: " 1>&2
 # Temporary files
+echo -n "." 1>&2
 ebuild_list=$(mktemp -t ebuild.XXXXXX)
+echo -n "." 1>&2
 eclass_list=$(mktemp -t eclass.XXXXXX)
+echo -n "." 1>&2
 ebuild_grep_cmd=$(mktemp -t ebuild_grep_cmd.XXXXXX)
+echo " done." 1>&2
 
 pushd $PORTDIR >/dev/null
 
-echo "Generating eclass list:" 1>&2
+echo -n "Generating eclass list: " 1>&2
 
 # Generate list of matching eclasses
 for ec in eclass/*.eclass ; do
 	# Skip this false positive
 	[[ "$ec" == *eutils.eclass ]] && continue
-	grep -e enewuser -e enewgroup $ec >/dev/null 2>&1 \
-		&& echo $ec >> $eclass_list
+	if grep -l -e enewuser -e enewgroup $ec >> $eclass_list ; then
+		echo -n "+" 1>&2
+	else
+		echo -n "." 1>&2
+	fi
 done
+echo " done." 1>&2
 
-echo "Generating ebuild grep command:" 1>&2
-
+echo -n "Generating ebuild grep command: " 1>&2
 # Build the arguments for our grep of each ebuild, so we only have to touch
 # each ebuild once.
-echo -n "-e enewuser -e enewgroup" >> $ebuild_grep_cmd
+echo -n "-l -e enewuser -e enewgroup" >> $ebuild_grep_cmd
 for ec in $(< $eclass_list) ; do
 	ec=${ec/eclass\/}
 	ec=${ec/.eclass}
+	echo -n "." 1>&2
 	echo -n " -einherit.\\\\*$ec" >> $ebuild_grep_cmd
 done
 echo >> $ebuild_grep_cmd
+echo " done." 1>&2
 
-echo "Generating ebuild list:" 1>&2
-
+echo -n "Generating ebuild list: " 1>&2
+i=0
 # Generate list of matching ebuilds
 for c in $(< profiles/categories) ; do
 	for e in $c/*/*.ebuild ; do
-		grep $(< $ebuild_grep_cmd) $e >/dev/null 2>&1 \
-			&& echo $e >> $ebuild_list
+		[[ -f "${e}" ]] || continue
+		i=$(( $i + 1 ))
+		[[ $(( $i % 100 )) == 0 ]] && echo -n "." 1>&2
+		grep $(< $ebuild_grep_cmd) $e >> $ebuild_list \
+			&& echo -n "+" 1>&2
 	done
 done
+echo " done." 1>&2
 
-echo "Generating final list:" 1>&2
-
+echo "Generating final list: " 1>&2
 # Generate our final list
-cat $ebuild_list |sort -u
+cat $eclass_list $ebuild_list
+echo " done." 1>&2
 
 popd >/dev/null
 rm -f $ebuild_list $eclass_list $ebuild_grep_cmd
